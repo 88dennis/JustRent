2997c727a167b9d31b0ed086557b84b19f62f465
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2020-04-04 18:15:36 +0800

    up

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ let bodyParser = require("body-parser");
 let PORT = 3000;
 let app = express();
 let rentalObjects = [
-    {name:"Generator Set", description: "10KVA", price: 1200, imgSource:""},
-    {name:"Industrial fan", description: "Stand Fan", price: 1900, imgSource:""},
-    {name:"Air cooler", description: "fan coolers", price: 2500, imgSource:""},
-    {name:"Jack Hammer", description: "Portable", price: 800, imgSource:""},
-    {name:"Welding Machine", description: "Portable",price: 900, imgSource:""},
-    {name:"Coring Machine", description: "Hilti Brand", price: 5000, imgSource:""},
+    {name:"Generator Set", description: "10KVA", price: 1200, imgSource:"/assets/images/generator.jpg"},
+    {name:"Industrial fan", description: "Stand Fan", price: 1900, imgSource:"/assets/images/industrialFan.jpg"},
+    {name:"Air cooler", description: "fan coolers", price: 2500, imgSource:"/assets/images/aircooler.jpg"},
+    {name:"Jack Hammer", description: "Portable Jackhammer", price: 800, imgSource:"/assets/images/jackhammer.jpg"},
+    {name:"Welding Machine", description: "Portable Welding Machine",price: 900, imgSource:"assets/images/weldingmachine.jpg"},
+    {name:"Coring Machine", description: "Portable Coring Machine", price: 5000, imgSource:"assets/images/coringmachine.jpg"},
 ]
-console.log(rentalObjects);
+// console.log(rentalObjects);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
-
 
 app.get("/", function(req, res){
     res.render("landingPage");
